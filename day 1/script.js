1ded27288de5acdb4144ea9fc66b5d95ba287210
Cody <codykoscielski@gmail.com>
2020-12-11 19:01:06 -0500

    Finished day one, expanding cards

---
+++ day 1/script.js
@@ -0,0 +1,15 @@
+//Grab the panel
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    });
+};
