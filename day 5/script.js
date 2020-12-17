078b460d63e5945e49a7e6daed8ad9e29a5da56d
Cody <codykoscielski@gmail.com>
2020-12-16 19:58:43 -0500

    Day 5

---
+++ day 5/script.js
@@ -0,0 +1,27 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+    load++;
+
+    if(load > 99) {
+        clearInterval(int);
+    }
+
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+
+//Num is inital load
+//inmin is the min number
+//inmax is the max number
+//outmin is the start or end depending on if background or text
+//outmax is the start or end, depening on background or text
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
